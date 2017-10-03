616ca921ca87c3e74fbe193b33c2384e4ae41d33
supermarioknight <hunter.dev.us@gmail.com>
2017-10-03 20:17:21 +1100

    chore(deps): updates deps

--- stories/index.js
+++ stories/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Component from '../src/Component';
 
-import '../src/style.less';
+import '../src/styles.less';
 
 storiesOf('Component')
   .add('default', () => <Component initialCount={2} />);
